d1ceb550f16ca266f4fedb272d497e6fe30a39dd
Lehan Gajanayake <gajanayakelehan@gmail.com>
2020-05-06 13:25:00 +0530

    Initial commit 2

---
+++ Mobile/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack'
+import {Map} from './screens/Map';
+import {Home} from './screens/Home';
+import {Details} from './screens/Details'
+
+
+
+const  MainNavigator = createStackNavigator({
+  Home: {screen: Home},
+  Map: {screen: Map},
+  Details: {screen: Details}
+});
+
+const App = createAppContainer(MainNavigator);
+export default App;
